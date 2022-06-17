1e36e6fd12ffc77b4fc071d526b0b0fd3f93b86f
Virgile AE <ae.virgile@gmail.com>
2022-06-17 17:02:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+
+
+def make_map(level):
+    num = level + 2 
+    default = [ [0 for _ in range(num)] for _ in range(level)]
+    return default
+
+
+def make_rand_road(grid):
+    orientations = ['vertical', 'horizontal', 'neg_perpendicular', 'pos_']
+    pass
+
+make_map(3)
---
+++ plan.md
@@ -0,0 +1,13 @@
+Gameplay
+========
+
+- There are levels which get progressively harder.
+- Each level is randomly generated so that the game remains interesting.
+- 
+
+Map
+===
+
+Generation
+----------
+
